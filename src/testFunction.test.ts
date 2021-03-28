1227e965a0b20f741b630a9b580ac03ba0e4117a
Kei Ito <kei-ito@users.noreply.github.com>
2021-03-28 11:35:32 +0900

    deps: upgrade dependencies (#112)
    * deps: @nlib/eslint-config:3.17.16→3.17.21 @nlib/lint-commit:0.1.3→0.1.4 @nlib/nodetool:0.1.11→0.1.16 @types/node:14.14.35→14.14.37 eslint:7.22.0→7.23.0
    
    * style: fix eslint errors

--- src/testFunction.test.ts
+++ src/testFunction.test.ts
@@ -1,8 +1,6 @@
 import {testFunction} from './testFunction';
 
-const singleSyncTestee = (
-    input: number,
-) => {
+const singleSyncTestee = (input: number) => {
     if (input < 10) {
         return input * 2;
     }
@@ -11,9 +9,7 @@ const singleSyncTestee = (
 testFunction(singleSyncTestee, {input: 5, expected: 10});
 testFunction(singleSyncTestee, {input: 10, error: {message: 'Input:10'}});
 
-const singleLikeSyncTestee = (
-    input: number,
-) => {
+const singleLikeSyncTestee = (input: number) => {
     if (input < 10) {
         return {result1: input, result2: input * 2, result3: input * 3};
     }
@@ -22,10 +18,7 @@ const singleLikeSyncTestee = (
 testFunction(singleLikeSyncTestee, {input: 5, like: {result1: 5, result2: 10}});
 testFunction(singleLikeSyncTestee, {input: 10, error: {message: 'Input:10'}});
 
-const multipleSyncTestee = (
-    input1: number,
-    input2: number,
-) => {
+const multipleSyncTestee = (input1: number, input2: number) => {
     if (input1 < 10) {
         return input1 + input2;
     }
@@ -34,9 +27,7 @@ const multipleSyncTestee = (
 testFunction(multipleSyncTestee, {parameters: [1, 2], expected: 3});
 testFunction(multipleSyncTestee, {parameters: [10, 11], error: {message: 'Input:10,11'}});
 
-const singleAsyncTestee = async (
-    input: number,
-) => {
+const singleAsyncTestee = async (input: number) => {
     await Promise.resolve(input);
     if (input < 10) {
         return input * 2;
@@ -46,10 +37,7 @@ const singleAsyncTestee = async (
 testFunction(singleAsyncTestee, {input: 5, expected: 10});
 testFunction(singleAsyncTestee, {input: 10, error: {message: 'Input:10'}});
 
-const multipleAsyncTestee = async (
-    input1: number,
-    input2: number,
-) => {
+const multipleAsyncTestee = async (input1: number, input2: number) => {
     await Promise.resolve(input1);
     if (input1 < 10) {
         return input1 + input2;
